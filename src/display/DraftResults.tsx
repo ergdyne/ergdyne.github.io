062c2f17cff4a92ff2ffc34275c18b2f7d409aa6
Jason Bennett <jason@ergdyne.com>
2022-05-07 18:07:18 +0700

    add position change, may have but with changing multiple

--- src/display/DraftResults.tsx
+++ src/display/DraftResults.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { Citizen } from '../mockAPI/citizen'
 import DistrictCitizens from './DistrictCitizens'
 
 interface Props {
   draftResults: Citizen[][]
+  updateCitizen: (district: number, citizen: Citizen) => void
+  disabled: boolean
 }
 
 export default function DraftResults(props: Props) {
+  const { draftResults, updateCitizen, disabled } = props
   return (<div>
-    { props.draftResults.map((citizens, i) => <DistrictCitizens
-        key = {i}
-        citizens = {citizens}
-        districtNumber = {i + 137}
-      />)
+    { draftResults.map((citizens, i) => {
+        const citizenUpdate = useCallback(
+          (citizen: Citizen) => { updateCitizen(i, citizen) },
+          [citizens, i]
+        )
+        return (
+          <DistrictCitizens
+            key = {i}
+            citizens = {citizens}
+            districtNumber = {i + 137}
+            updateCitizen={citizenUpdate}
+            disabled={disabled}
+          />
+        )
+      })
     }
   </div>)
 }
